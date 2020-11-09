54e67bb06816235ec92a915c6dfcb377d91930c9
Kseniya Tarasevich <tarasevichkseniya11@gmail.com>
2020-11-09 19:59:37 +0300

    simple turtle game

---
+++ Turtle/Turtle/TurtleGame.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.SmallBasic.Library;
+
+namespace TurtleTraining
+{
+    class TurtleGame
+    {
+        static void WriteT()
+        {
+            Turtle.Move(80);
+            Turtle.TurnLeft();
+            Turtle.Move(30);
+            Turtle.Turn(180);
+            Turtle.Move(60);
+        }
+
+        static void WriteO(int size)
+        {
+            Turtle.Angle = 0;
+            int i = 0;
+            while (i < 8)
+            {
+                Turtle.Move(size);
+                Turtle.Turn(45);
+                i++;
+            }
+        }
+
+        static void WriteR()
+        {
+            Turtle.Angle = 0;
+            Turtle.Move(80);
+            Turtle.TurnRight();
+            int i = 0;
+            while (i < 5)
+            {
+                Turtle.Move(22);
+                Turtle.Turn(45);
+                i++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Turtle.Speed = 10;
+            Turtle.X = 200;
+            Turtle.Y = 200;
+
+            WriteT();
+
+            Turtle.X = 260;
+            Turtle.Y = 170;
+
+            WriteO(30);
+
+
+            Turtle.X = 450;
+            Turtle.Y = 200;
+
+            WriteT();
+
+            Turtle.X = 365;
+            Turtle.Y = 200;
+
+            WriteR();
+            //Turtle.Move(100);
+
+            /* for (int j = 0; j < 4; j++)
+             {
+                 Turtle.Move(30);
+                 Turtle.TurnRight();
+                 Turtle.Move(30);
+                 Turtle.TurnRight();
+                 Turtle.Move(30);
+                 Turtle.TurnLeft();
+                 Turtle.Move(30);
+                 Turtle.TurnLeft();
+
+             }
+
+             int i = 0;
+             while (i < 6)
+             {
+                 Turtle.Move(100);
+                 Turtle.Turn(60);
+                 i++;
+             }*/
+        }
+    }
+}
---
+++ Turtle/TurtleGame/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.SmallBasic.Library;
+using System;
+
+namespace TurtleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Turtle.PenUp();
+            GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
+
+            GraphicsWindow.BrushColor = "Red";
+            var food = Shapes.AddRectangle(10, 10);
+
+            int xFoodPosition = 200;
+            int yFoodPosition = 200;
+
+            Shapes.Move(food, xFoodPosition, yFoodPosition);
+
+            Random rand = new Random();
+
+            while (true)
+            {
+                Turtle.Move(10);
+
+                if (Turtle.X >= xFoodPosition && Turtle.X <= xFoodPosition + 10 && Turtle.Y >= yFoodPosition && Turtle.Y <= yFoodPosition + 10)
+                {
+                    xFoodPosition = rand.Next(0, GraphicsWindow.Width);
+                    yFoodPosition = rand.Next(0, GraphicsWindow.Height);
+                    Shapes.Move(food, xFoodPosition, yFoodPosition);
+                    Turtle.Speed += 1;
+                }
+            }
+        }
+
+        private static void GraphicsWindow_KeyDown()
+        {
+            if (GraphicsWindow.LastKey == "Up")
+            {
+                Turtle.Angle = 0;
+            }
+
+            else if (GraphicsWindow.LastKey == "Right")
+            {
+                Turtle.Angle = 90;
+            }
+
+            else if (GraphicsWindow.LastKey == "Left")
+            {
+                Turtle.Angle = 270;
+            }
+
+            else if (GraphicsWindow.LastKey == "Down")
+            {
+                Turtle.Angle = 180;
+            }
+        }
+    }
+}
