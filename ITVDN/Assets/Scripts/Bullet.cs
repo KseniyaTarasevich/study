32bcd33f3c683739e42749a6bb748ea82af431f3
Kseniya Tarasevich <tarasevichkseniya11@gmail.com>
2021-01-08 21:14:59 +0300

    first commit

---
+++ ITVDN/Assets/Scripts/Bullet.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private GameObject parent;
+    private Vector3 direction;
+    public GameObject Parent
+    {
+        set
+        {
+            parent = value;
+        }
+    }
+
+    [SerializeField] private float _speed;
+    
+
+    private void Start()
+    {
+        Destroy(gameObject, 1.4f);
+    }
+
+    public Vector3 Direction
+    {
+        set
+        {
+            direction = value;
+        }
+    }
+
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Unit unit = collider.GetComponent<Unit>();
+
+        if (unit && unit.gameObject != parent)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+}
+
---
+++ ITVDN/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 2f;
+
+    [SerializeField] private Transform target;
+
+    private void Awake()
+    {
+        if(!target)
+        {
+            target = FindObjectOfType<Character>().transform;
+        }
+    }
+
+    private void Update()
+    {
+
+        Vector3 position = target.position;
+        position.z = -10.0f;
+        transform.position = Vector3.Lerp(transform.position, position, _speed * Time.deltaTime);
+
+    }
+}
